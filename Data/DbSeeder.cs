9fa4e2854f67dbfd5482f4db4a68d1303ff51e52
fahad14al <fahadalmohammad50@gmail.com>
2026-05-14 18:30:50 +0600

    seeding data

---
+++ Data/DbSeeder.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.NetworkInformation;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+namespace Server.Data
+
+
+{
+    public class DbSeeder
+    {
+        public static async Task SeedAsync(AppDbContext _dbcontext)
+        {
+            await _dbcontext .Database.MigrateAsync();
+
+            //seed Roles
+            if (!_dbcontext.Roles.Any())
+            {
+                var roles = new List<Role>
+                    {
+                        new Role { RoleName = "Admin" },
+                        new Role { RoleName = "Developer" },
+                        new Role { RoleName = "Tester" },
+                        new Role { RoleName = "Manager" }
+                    };
+                _dbcontext.Roles.AddRange(roles);
+                await _dbcontext.SaveChangesAsync();
+
+            }
+
+            //seed Users
+            if (!_dbcontext.Users.Any())
+            {
+                var adminRole = await _dbcontext.Roles.FirstOrDefaultAsync(r => r.RoleName == "Admin");
+                // create users (remove RoleId usage)
+                var users = new[]
+                {
+                    new User { Username = "admin", UserEmail = "admin@example.com" },
+                    new User { Username = "dev1",  UserEmail = "dev1@example.com" },
+                    new User { Username = "dev2",  UserEmail = "dev2@example.com" },
+                    new User { Username = "Tester", UserEmail = "Tester@example.com" },
+                    new User { Username = "Manager", UserEmail = "Manager@example.com" }
+                };
+
+                _dbcontext.Users.AddRange(users);
+                await _dbcontext.SaveChangesAsync();
+
+                // create user-role links (use RoleId on Role)
+                var userRoles = users.Select(u => new UserRole
+                {
+                    UserId = u.UserId,
+                    RoleId = adminRole.RoleId
+                }).ToArray();
+
+                _dbcontext.UserRoles.AddRange(userRoles);
+                await _dbcontext.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,12 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await DbSeeder.SeedAsync(dbContext);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
